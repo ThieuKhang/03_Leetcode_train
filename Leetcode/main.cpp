f083573ec35f604f4ae2ef777debdb539c7fe566
ThieuKhang <165531664+ThieuKhang@users.noreply.github.com>
2024-10-19 19:04:51 +0700

    main

--- Leetcode/main.cpp
+++ Leetcode/main.cpp
@@ -1,6 +1,46 @@
 #include <iostream>
+#include <stack>
+
+
+class Solution {
+public:
+    char closeToOpen(char c) {
+        if(c == ')') {
+            return '(';
+        } else if(c == '}') {
+            return '{';
+        } else 
+            return '[';
+    }
+
+    bool isOpen(char c) {
+        if(c == '(' || c == '[' || c == '{')
+            return true;
+        return false;
+    }
+
+    bool isValid(std::string s) {
+        std::stack<char> myStack;
+        for(char c : s) {
+            if(isOpen(c)) {
+                myStack.push(c);
+                continue;
+            }
+            if(myStack.top() != closeToOpen(c)) {
+                return false;
+            } else
+                myStack.pop(); 
+                continue;
+        }
+        return true;
+    }
+};
+
 
 int main() {
-
+    Solution Dec19;
+    std::string s = "(]";
+    bool outPut = Dec19.isValid(s);
+    std::cout << outPut << std::endl;
     return 0;
 }
